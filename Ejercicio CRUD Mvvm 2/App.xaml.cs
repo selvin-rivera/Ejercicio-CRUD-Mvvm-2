2f7d4f83b4240174f6f29893c6454af60baa1417
selvin-rivera <nrivera.clements@gmail.com>
2024-06-17 17:23:36 -0600

    Add project files.

---
+++ Ejercicio CRUD Mvvm 2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio_CRUD_Mvvm_2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Ejercicio CRUD Mvvm 2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio_CRUD_Mvvm_2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
